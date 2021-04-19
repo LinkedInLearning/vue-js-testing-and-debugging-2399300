f334d465718394fa423e957f56e424fdccbda67a
Fikayo <fikfikky@gmail.com>
2021-04-19 12:14:20 +0100

    Login Page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,81 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  
+      <div class="container">
+        <!-- <div class="row">
+          <div class="col-md-12">
+            <img src="/logo.gif" />
+          </div>
+        </div> -->
+        <div class="row">
+            <div class="col-md-8" id="banner-section">
+
+            </div>
+            <div class="col-md-4">
+              <div class="row">
+                <div class="col-md-12">
+                  <div class="col-md-12">
+                    <img src="/logo.gif" height="120" class="float-right" />
+                  </div>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-md-12">
+                  <div class="card mt-4">
+                    <div class="card-header bg-primary text-white">
+                      Login to your account
+                    </div>
+
+                    <div class="card-body">
+                      <form>
+                        <div>
+                          <div class="form-group">
+                            <input type="email" placeholder="Enter Email" class="form-control">
+                          </div>
+                          <div class="form-group">
+                            <input type="password" placeholder="Enter Password" class="form-control">
+                          </div>
+                          <button type="button" class="btn btn-primary">
+                            Login
+                          </button>
+                        </div>
+                      </form>
+                    </div>
+                    
+                  </div>
+                </div>
+              </div>
+              
+              
+              
+              
+            </div>
+        </div>
+
+    
+    </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    
   }
 }
 </script>
+
+<style scoped>
+.container{
+  padding: 0;
+  margin: 0;
+}
+
+
+#banner-section{
+  background-image: url("/home_page_back.jpg");
+  padding: 0;
+  margin: 0;
+  height: 100vh;
+}
+</style>
