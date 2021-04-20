1dcf1317b9b07e8528250d21659dd8181b0de59b
Fikayo <fikfikky@gmail.com>
2021-04-20 11:27:49 +0100

    Login to profile

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -34,7 +34,7 @@
                           <div class="form-group">
                             <input type="password" placeholder="Enter Password" class="form-control">
                           </div>
-                          <button type="button" class="btn btn-primary">
+                          <button type="button" class="btn btn-primary" @click="login()">
                             Login
                           </button>
                         </div>
@@ -61,6 +61,11 @@ export default {
   name: 'Home',
   components: {
     
+  },
+  methods : {
+    login(){
+      this.$router.push({path : "profile"})
+    }
   }
 }
 </script>
