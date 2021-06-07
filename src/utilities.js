07f917addeca767273dcbae6587d10956973c7c5
Fikayo <fikfikky@gmail.com>
2021-06-07 12:24:41 +0100

    Testing objects and arrays - Starting point

--- src/utilities.js
+++ src/utilities.js
@@ -2,6 +2,23 @@ function addNumbers(a, b) {
     return a + b;
 }
 
+let countriesCollection = [
+    'United States',
+    'United Kingdom',
+    'Nigeria',
+    'India',
+    'China'
+]
+
+let johnDoe = {
+    name: "John Doe",
+    age: 26,
+    sex : "male",
+    occupation: "Accountant"
+}
+
 module.exports = {
-    addNumbers
+    addNumbers,
+    countriesCollection,
+    johnDoe
 }
