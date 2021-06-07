1ec9d26aa42d28459780bd78adf27781eba1fb5d
Fikayo <fikfikky@gmail.com>
2021-06-07 18:57:16 +0100

    Accessing Elements - Completed

--- src/__tests__/mount.js
+++ src/__tests__/mount.js
@@ -5,4 +5,6 @@ test('Render Home', async () => {
     // The render method returns a collection of utilities to query your component.
     const { getByText } = render(Home);
 
+    getByText("Login to your account");
+
 })
