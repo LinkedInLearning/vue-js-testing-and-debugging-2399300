7e02b5f7fe0a0479783a9a77117823c1236b804d
Fikayo <fikfikky@gmail.com>
2021-06-07 18:00:54 +0100

    Stashed

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+module.exports =  {
+    "moduleFileExtensions": [
+        "js",
+        "json",
+        // tell Jest to handle `*.vue` files
+        "vue"
+    ],
+    "transform": {
+        ".*\\.(js)$": "babel-jest",
+        // process `*.vue` files with `vue-jest`
+        ".*\\.(vue)$": "vue-jest"
+    }
+}
+  
