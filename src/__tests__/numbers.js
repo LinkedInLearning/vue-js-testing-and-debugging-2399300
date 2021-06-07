46dca2fc9e4ebce8aba7a066e7c09fb62a34ea2a
Fikayo <fikfikky@gmail.com>
2021-06-07 12:13:55 +0100

    Testing Numbers - Complete

--- src/__tests__/numbers.js
+++ src/__tests__/numbers.js
@@ -1,5 +1,9 @@
-import {addNumbers } from "../utilities";
+import {addNumbers} from "../utilities";
 
 test ("check the addition of numbers", () => {
     expect(addNumbers(4, 5)).toEqual(9);
+})
+
+test ("adding floating point numbers", () => {
+    expect(addNumbers(0.1, 0.2)).toBeCloseTo(0.3);
 })
